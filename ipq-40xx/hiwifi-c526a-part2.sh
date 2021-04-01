dad09825806060a5dcb0989e9c00809f4e40d39a
mask0719 <77974131+mask0719@users.noreply.github.com>
2021-04-01 14:39:25 +0800

    Update hiwifi-c526a-part2.sh

--- ipq-40xx/hiwifi-c526a-part2.sh
+++ ipq-40xx/hiwifi-c526a-part2.sh
@@ -58,6 +58,7 @@ CONFIG_PACKAGE_luci-app-vlmcsd=y
 CONFIG_PACKAGE_luci-app-turboacc=y
 CONFIG_PACKAGE_luci-app-ipsec-vpnd=y
 CONFIG_PACKAGE_ddns-scripts_dnspod=y
+CONFIG_PACKAGE_v2ray=y
 EOF
 
 # 常用软件 默认已启用
